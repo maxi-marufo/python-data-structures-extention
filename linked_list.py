bd907af726d9e72100df546978187fe0e051bdfd
Maxi Marufo <31772558+maxi-marufo@users.noreply.github.com>
2019-08-11 17:27:46 -0300

    ADD insert_first and delete_first methods

--- linked_list.py
+++ linked_list.py
@@ -53,3 +53,17 @@ class LinkedList(object):
                 previous.next = current.next
             else:
                 self.head = current.next
+
+    def insert_first(self, new_element):
+        new_element.next = self.head
+        self.head = new_element
+
+    def delete_first(self):
+        if self.head:
+            deleted_element = self.head
+            temp = deleted_element.next
+            self.head = temp
+            return deleted_element
+        else:
+            return None
+                
