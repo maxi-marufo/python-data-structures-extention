3d1ee6e0566560e7f407126cc4204f238e4ac91e
Maxi Marufo <31772558+maxi-marufo@users.noreply.github.com>
2019-08-11 17:28:40 -0300

    Create stack.py

---
+++ stack.py
@@ -0,0 +1,11 @@
+import linked_list as LinkedList
+
+class Stack(object):
+    def __init__(self,top=None):
+        self.ll = LinkedList(top)
+
+    def push(self, new_element):
+        self.ll.insert_first(new_element)
+
+    def pop(self):
+        return self.ll.delete_first()
