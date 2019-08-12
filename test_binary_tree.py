b67bc66fc53de8bb1dc74ccb4ec634d1f9f73e3f
Maxi Marufo <31772558+maxi-marufo@users.noreply.github.com>
2019-08-12 00:49:00 -0300

    Create test_binary_tree.py

---
+++ test_binary_tree.py
@@ -0,0 +1,18 @@
+import binary_tree
+
+# Set up tree
+tree = BinaryTree(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+
+# Test search
+# Should be True
+print tree.search(4)
+# Should be False
+print tree.search(6)
+
+# Test print_tree
+# Should be 1-2-4-5-3
+print tree.print_tree()
